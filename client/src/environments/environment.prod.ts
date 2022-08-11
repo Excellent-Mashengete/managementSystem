cf1c696145c703730b58f6013827ea078b7855a3
Excellent <excellent.mashengete@dadevs.co.za>
2022-08-11 22:42:45 +0200

    hosted backen

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 //const baseUrl = "http://localhost:7070/api/"
 
-const baseUrl = "http://localhost:7070/api/"
+const baseUrl = "https://hrempmanagement.herokuapp.com/api/"
 
 export const environment = {
   production: true,
