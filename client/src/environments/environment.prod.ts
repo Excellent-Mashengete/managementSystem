d091efe2e94f8dfbdb7683b212593392e95117bf
Excellent <excellent.mashengete@dadevs.co.za>
2022-08-14 23:01:59 +0200

    added new column statu

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
-const baseUrl = "http://localhost:7070/api/"
-
-//const baseUrl = "https://hrempmanagement.herokuapp.com/api/"
+//const baseUrl = "http://localhost:7070/api/"
+const baseUrl = "https://hrempmanagement.herokuapp.com/api/"
 
 export const environment = {
   production: true,
